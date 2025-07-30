4afb5f53cf332f7c1c21b28d0b5378b083402d39
shamsudheen <shamsu1997@gmail.com>
2025-07-30 06:17:02 +0530

    fix: add speaker reg link

--- src/app/speaker/page.tsx
+++ src/app/speaker/page.tsx
@@ -397,7 +397,9 @@ export default function Speaker() {
 					{/* Register Button */}
 					<div className="text-center mt-12">
 						<a
-							href="#"
+							href="https://forms.gle/GXaRRMBd46XwA7bB8"
+							target="_blank"
+							rel="noreferrer"
 							className="mt-auto w-full max-w-xs mx-auto bg-gradient-to-br from-[#1d1d1f] to-[#2d2d2f] hover:from-[#2d2d2f] hover:to-[#3d3d3f] text-white text-[15px] py-3 font-medium text-center rounded-full transition-all duration-200 shadow-sm hover:shadow-md flex items-center justify-center gap-2 uppercase"
 						>
 							Register as a Speaker
